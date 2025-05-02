c228e285174e1d8d8da723703db3e7953098f321
Prasanna-Shrestha <prasanna.shrestha0318@gmail.com>
2025-05-02 09:22:38 +0545

    Added json file and made few changes for model deployement in Vercel

--- app.py
+++ app.py
@@ -10,15 +10,12 @@ import uuid
 from dotenv import load_dotenv
 import os
 
-app = Flask(__name__)
-
-# Configuration    
-load_dotenv()  # load from .env
+app = Flask(__name__, static_url_path='/static')
 
 cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
     secure=True
 )
 
